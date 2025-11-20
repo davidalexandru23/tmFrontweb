ba99e4c9bc3552f31a8baefda0fc78c987b07345
{username}David <{email}davidalexandru23@stud.ase.ro>
2025-11-20 16:17:39 +0200

    fix: add missing foundation import for kIsWeb

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // For kIsWeb
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
